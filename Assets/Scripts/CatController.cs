7fed816112cc6d8e4b23d6d9b9e973959d0918f9
edvardius <mtyszkiewicz@gmail.com>
2017-01-21 00:01:19 +0100

    bucefał

--- Assets/Scripts/CatController.cs
+++ Assets/Scripts/CatController.cs
@@ -1,16 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CatController : MonoBehaviour {
+public class CatController : MonoBehaviour
+{
+	// HOOKS FOR WAVES
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			// render high pitch wave
+			throw new Exception("Glass breaker");
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			// render medium pitch wave
+			throw new Exception("DEMEXICANIZER");
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			// render high pitch wave
+			throw new Exception("Wall destroyer");
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha4))
+		{
+			// render high pitch wave
+			throw new Exception("OGÓREK PSIK");
+		}
 	}
 }
